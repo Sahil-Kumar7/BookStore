15806228f973ae5c34b468e1adfd1ccde8fc72d1
sahil <sahil1223.be21@chitkara.edu.in>
2024-02-02 19:23:36 +0530

    another commit

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -22,7 +22,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post('http://localhost:5555/books', data)
+      .post('https://book-management-system-umbb.onrender.com/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created successfully', { variant: 'success' });
